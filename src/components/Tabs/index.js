c0f47aa4dced413879df07e1a129e586faa6057c
ceyang <ceyanglab@gmail.com>
2022-09-23 11:15:08 +0800

    feat: Tabs component create complete

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,9 +1,45 @@
-export const Tabs = (props = []) => {
-  let testProps = [
-    {
-      title: "schedule",
-      pageElement: document.createElement("div"),
-    },
-  ];
-  let div = document.createElement("div");
+let defaultTabs = [
+  { title: "Tab1", pageElement: document.createElement("div") },
+  { title: "Tab2", pageElement: document.createElement("div") },
+  { title: "Tab3", pageElement: document.createElement("div") },
+];
+
+export const Tabs = (props = defaultTabs) => {
+  let div_root = document.createElement("div");
+  let div_tabs = document.createElement("div");
+  let div_tabPane = document.createElement("div");
+
+  div_root.className = "tabs-container";
+  div_tabs.className = "tabs";
+
+  // 產生 tabs
+  for (let i = 0; i < props.length; i++) {
+    const element = props[i];
+    let div_tab = document.createElement("div");
+
+    div_tab.id = "tab_" + element.title;
+    div_tab.className = "tab";
+    div_tab.innerHTML = element.title;
+
+    div_tab.addEventListener("click", () => {
+      // 清除 page
+      div_tabPane.innerHTML = "";
+
+      // 清除上一個選擇狀態,將目前點擊的tab轉為已選擇
+      let selected = document.querySelector(".tab-active");
+      if (selected) {
+        selected.classList.remove("tab-active");
+      }
+      div_tab.classList.add("tab-active");
+
+      // 加入 page
+      div_tabPane.appendChild(element.pageElement);
+    });
+    div_tabs.appendChild(div_tab);
+  }
+
+  div_root.appendChild(div_tabs);
+  div_root.appendChild(div_tabPane);
+
+  return div_root;
 };
